8a1586197202704c79ae246d98a9d18d4216ac1e
B0Bu4 <bobu41@yandex.ru>
2022-05-30 14:03:02 +0300

    message

--- src/main/java/crud/config/WebAppInitializer.java
+++ src/main/java/crud/config/WebAppInitializer.java
@@ -7,7 +7,7 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected Class<?>[] getRootConfigClasses() {
 
-        return new Class[0];
+        return null;
     }
 
     @Override
--- src/main/java/crud/model/User.java
+++ src/main/java/crud/model/User.java
@@ -17,13 +17,21 @@ public class User {
     @Column(name = "last_name")
     private String lastname;
 
+    @Column(name = "age")
+    private byte age;
+
+    @Column(name = "email_address")
+    private String email;
+
     public User() {
     }
 
-    public User(Long id, String name, String firstname) {
+    public User(Long id, String firstname, String lastname, byte age, String email) {
         this.id = id;
-        this.firstname = name;
-        this.lastname = firstname;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.email = email;
     }
 
     public Long getId() {
@@ -50,26 +58,45 @@ public class User {
         this.lastname = firstname;
     }
 
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(firstname, user.firstname) &&
-                Objects.equals(lastname, user.lastname);
+        return age == user.age && Objects.equals(id, user.id) && Objects.equals(firstname, user.firstname) &&
+                Objects.equals(lastname, user.lastname) && Objects.equals(email, user.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstname, lastname);
+        return Objects.hash(id, firstname, lastname, age, email);
     }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", name='" + firstname + '\'' +
-                ", firstname='" + lastname + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
